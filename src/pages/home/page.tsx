fd3aa90907052f1c6a90e0a85bc6e702306e04ad
Takahiro-Ikeda <ikeda@closip.co.jp>
2026-01-23 17:24:29 +0900

    Update from Readdy: 変更内容の簡単な説明（例: 事業内容テキスト修正）

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -144,7 +144,7 @@ export default function HomePage() {
           {/* Banner Image */}
           <div className="mt-12 flex justify-center">
           <img
-          src="https://drive.google.com/file/d/1HS_APYuOQs4YgLvRZZgcMXkcqAuz2o_s/view?usp=sharing"
+          src="https://drive.google.com/uc?export=view&id=1HS_APYuOQs4YgLvRZZgcMXkcqAuz2o_s"
           alt="生成AI実務研究会バナー"
           className="w-full max-w-5xl rounded-2xl shadow-2xl"
           />
